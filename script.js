65bd96850d8e58451259010fa8b874c60ffd78a8
OsmanRodrigues <osmancesar.mr@gmail.com>
2020-09-04 09:52:35 -0300

    alteração na msg do console log

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 (() => {
     console.log("Serei o conflito?!");
+    console.log("! ogidoc ed em wohS");
 })();
+
+//1. Deixo o código do Matheus
+//2. Deixo o meu código
+//3. Deixo os dois (WINNER)
